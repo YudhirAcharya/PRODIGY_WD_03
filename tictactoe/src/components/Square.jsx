0f85fd785b363eaa20471ea69316a42c8fb4afe2
Yudhir <121503369+YudhirAcharya@users.noreply.github.com>
2023-12-17 12:41:13 +0545

    setting board

---
+++ tictactoe/src/components/Square.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Square() {
+  return (
+    <div>
+      <div className="row1">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="row1">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="row1">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+    </div>
+  );
+}
